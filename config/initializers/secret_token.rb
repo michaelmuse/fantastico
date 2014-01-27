6ebc587543d5076a2ff81fc089cdbe5897295bec
Michael Muse <michaelmuse@gmail.com>
2014-01-27 13:42:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantastico::Application.config.secret_token = 'b5b67e431d5f0b3c1a3313d80b8bdea7228369685064516ddb618d6b768919340145738cf70332aee67d94c0848c54b83c2aa7379d284895f3ce1cf32ac0f804'
