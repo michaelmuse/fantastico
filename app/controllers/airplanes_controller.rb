e8dd7f22c814507d22ab1046e6e55f3122021be6
Michael Muse <michaelmuse@gmail.com>
2014-01-27 14:52:57 -0500

    adding the new and create stuff

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -1,6 +1,15 @@
 class AirplanesController < ApplicationController
 
-  def new  
+  def new
+  end
+  def create
+    Airplane.create({
+      name: params[:name],
+      manufacturer: params[:manufacturer],
+      passenger_capacity: params[:passenger_capacity],
+      year_made: params[:year_made]
+      })
+      redirect_to airplanes_path    
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Fantastico::Application.routes.draw do
+
+  resources :airplanes
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
